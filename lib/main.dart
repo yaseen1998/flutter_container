3baa7b5e6e058a67ec4e4f65d4aaa6046d68709d
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2022-08-31 14:57:25 +0300

    create scroll

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: double.infinity,
-          alignment: Alignment.center,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [Colors.white30,Colors.grey,Color.fromARGB(255, 222, 15, 201),Colors.pink]),
-            image: DecorationImage(
-              opacity: 80,
-              // fit: BoxFit.cover,
-              fit:BoxFit.scaleDown,
-              image: NetworkImage(
-                  "https://cdn.pixabay.com/photo/2022/08/27/16/42/flower-7414818_1280.jpg"),
+        body: SingleChildScrollView(
+          child:Container(
+          decoration: BoxDecoration(image: DecorationImage(
+            fit:BoxFit.fill,
+            image: NetworkImage('https://cdn.pixabay.com/photo/2022/08/25/14/09/church-7410428_1280.jpg'))),
+         child : Column(children: 
+         [
+          Container(
+            width: 200,
+            height: 500,
+            decoration:const BoxDecoration(
+              color:Color.fromARGB(255, 144, 108, 95),
+              image:DecorationImage(image: NetworkImage('https://media.istockphoto.com/photos/touch-of-fresh-moss-in-the-forest-picture-id1283852667'))
             ),
-          ),
-          child: const Text(
-            'Hello world',
-            style: TextStyle(fontSize: 50, color: Colors.purpleAccent,),
-          ),
+            child:const Text('dell',style:TextStyle(fontSize: 25))
+            ),
+            Divider(height: 50,color:Colors.blue,thickness: 5,),
+          Container(
+            width: 200,
+            height: 500,
+            decoration:const BoxDecoration(
+              image:DecorationImage(image: NetworkImage('https://media.istockphoto.com/photos/touch-of-fresh-moss-in-the-forest-picture-id1283852667'))
+            ),
+            child:const Text('toshiba',style:TextStyle(fontSize: 25))
+            ),
+            Center(
+              child:  Row(children:const [
+                SizedBox(width: 150,),
+                Card(
+                  elevation: 20,
+                  shadowColor: Colors.red,
+                  child: Text('buy now'),
+                  ),
+                Card(child: Text('add to favorite'),),
+              ]),
+            )
+              
+         ]
+         ,)
         ),
+          )
       ),
     );
   }
