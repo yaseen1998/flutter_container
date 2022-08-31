5837484b59f690528499bd4d81e8aeedcf07ac9b
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2022-08-31 14:16:25 +0300

    new image

--- lib/main.dart
+++ lib/main.dart
@@ -13,62 +13,23 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          color: Colors.blue,
+          height: double.infinity,
           alignment: Alignment.center,
-          child: Container(
-              alignment: Alignment.center,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-              width: 200,
-              height: 500,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 96, 4, 91),
-                    blurRadius: 10.0,
-                    spreadRadius: 6.0,
-                    offset: Offset(2.0, 10.0),
-                  ),
-                ],
-                border: Border(
-                  top: BorderSide(
-                      color: Color.fromARGB(255, 55, 54, 54), width: 2),
-                  bottom: BorderSide(
-                      color: Color.fromARGB(255, 27, 26, 26), width: 4),
-                  left: BorderSide(
-                      color: Color.fromARGB(255, 55, 34, 34), width: 6),
-                  right: BorderSide(
-                      color: Color.fromARGB(255, 69, 34, 34), width: 8),
-                ),
-              ),
-              child: Card(
-                  color: Colors.black,
-                  borderOnForeground: true,
-                  elevation: 40,
-                  child: Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     mainAxisSize: MainAxisSize.max,
-                    children: const [
-                      Text(
-                        'Hello World',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 228, 14, 193),
-                        ),
-                      ),
-                      Text(
-                        'This is a card',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 228, 14, 193),
-                        ),
-                      ),
-                      
-                    ],
-                  ))),
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.white30,Colors.grey,Color.fromARGB(255, 222, 15, 201),Colors.pink]),
+            image: DecorationImage(
+              opacity: 80,
+              // fit: BoxFit.cover,
+              fit:BoxFit.scaleDown,
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2022/08/27/16/42/flower-7414818_1280.jpg"),
+            ),
+          ),
+          child: const Text(
+            'Hello world',
+            style: TextStyle(fontSize: 50, color: Colors.purpleAccent,),
+          ),
         ),
       ),
     );
